caf29920ec40ed895dcf26cc211cf267fa7912f6
QolbinSR <qolbinsalimrayimura@smkwikrama.sch.id>
2021-12-21 08:03:31 +0630

    first commit

---
+++ first/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+      <cover msg="anjay"/>
+    </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import cover from './components/cover.vue'
+
+export default {
+  name: 'App',
+  components: {
+    cover
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Chivo&family=Dynalight&display=swap');
+#app{
+  font-family: Dynalight, Chivo;
+  text-align: center;
+  color: black;
+  margin-top: 6rem;
+}
+
+</style>
---
+++ first/src/components/cover.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="jumbotron">
+        <div class="container">
+            <h3>WITH GREAT PLEASURE</h3>
+            <h1>Invite You</h1>
+            <h3>To CELEBRATE OUR MARRIAGE</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'cover',
+}
+</script>
+
+<style scoped>
+    h3{
+        font-family: Chivo;
+    }
+    
+</style>
